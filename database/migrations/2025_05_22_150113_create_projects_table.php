66f51de9144a5fa71bd9eac7ab61dc7c7e9433fe
Szymon Niewiadomski <szymon.niewiadomski@flightcope.com>
2025-06-11 16:58:30 +0200

    fix descripton length

--- database/migrations/2025_05_22_150113_create_projects_table.php
+++ database/migrations/2025_05_22_150113_create_projects_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description')->nullable();
+            $table->text('description')->nullable();
             $table->string('url')->nullable();
             $table->string('github_url')->nullable();
             $table->integer('order')->nullable();
